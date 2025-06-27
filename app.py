a5ac1601e27a0ba324ac27cd1a7ea990b80e1765
Harshitha Bandepureddy <bandepureddyharshitha@gmail.com>
2025-06-27 17:06:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ authenticator = stauth.Authenticate(
     cookie_expiry_days=1
 )
 
-name, authentication_status, username = authenticator.login("Login", location="main")
+name, authentication_status, username = authenticator.login("Login", "main")
 
 if auth_status == False:
     st.error("Incorrect username or password")
